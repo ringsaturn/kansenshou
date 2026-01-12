ba51b5b9d1b3020b89bd8c82611c390e145f45d4
Han Xiao <ringsaturn.me@gmail.com>
2026-01-12 18:48:49 +0900

    init observable page

---
+++ web/observable/src/data/ari.parquet.js
@@ -0,0 +1,13 @@
+import * as duckdb from "npm:@duckdb/duckdb-wasm";
+
+const PARQUET_URL =
+  "https://kansenshou.ringsaturn.me/data/ari/merged_ari.parquet";
+
+const db = await duckdb.createDB();
+const conn = await db.connect();
+
+const result = await conn.query(`
+  SELECT * FROM read_parquet('${PARQUET_URL}')
+`);
+
+process.stdout.write(JSON.stringify(result.toArray()));
